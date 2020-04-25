3a8351cd1f457a21713e1ef2fab7f1b05682009d
Calvin C <lefrogjones@gmail.com>
2020-04-24 21:41:28 -0400

    finalizing lvl 3 background, changed scene fade in to normal update mode

---
+++ Assets/Scripts/LevelSelect/EnablePause.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enable pausing / toggling menu AFTER fade in has finished
+// Fade in must be using normal update mode so if game gets paused, it would be paused mid fade in
+// Attach to the fade in object and with an animation event, call EnableMenuToggle at end of animation
+
+public class EnablePause : MonoBehaviour
+{
+    [SerializeField]
+    MenuManager menu;
+
+    void Awake()
+    {
+        if (menu != null)
+        {
+            menu.enabled = false;
+        }
+    }
+
+    public void EnableMenuToggle()
+    {
+        if (menu != null)
+        {
+            menu.enabled = true;
+        }
+    }
+}
