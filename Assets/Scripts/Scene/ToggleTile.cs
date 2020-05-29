2ef82610b2c8db4164d26e7c7032442ae58229d4
Calvin C <lefrogjones@gmail.com>
2020-05-29 14:27:07 -0400

    setting up pyramid tile

---
+++ Assets/Scripts/Scene/ToggleTile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Toggles gameobject tile off and on upon entering/exiting collider this script is attached to
+
+public class ToggleTile : MonoBehaviour
+{
+    [SerializeField]
+    GameObject outsideTile;
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            outsideTile.SetActive(false);
+        }   
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        OnTriggerEnter2D(other);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            outsideTile.SetActive(true);
+        }
+    }
+
+}
