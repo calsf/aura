b8d4780f3e9ad0a68fb06bf52ca492d9c19ed7de
Calvin C <lefrogjones@gmail.com>
2020-05-30 15:12:36 -0400

    adding open chest behaviour, set up first pyramid

---
+++ Assets/Scripts/Scene/Chest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+// Opens chest and awards gold amount when player walks into the collider
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField]
+    int goldValue;
+
+    [SerializeField]
+    GameObject goldPopupPrefab;
+    GameObject goldPopup;
+    TextMeshPro goldPopupText;
+    LevelManager levelManager;
+
+    bool hasOpened;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+
+        goldPopup = Instantiate(goldPopupPrefab, Vector3.zero, Quaternion.identity);
+        goldPopupText = goldPopup.GetComponent<TextMeshPro>();
+    }
+
+    void Start()
+    {
+        // Deactive gold popup after
+        goldPopup.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && !hasOpened)
+        {
+            hasOpened = true;
+            anim.Play("ChestOpen");
+
+            goldPopupText.text = "+" + goldValue + " Gold";
+            goldPopup.transform.position = transform.position + (Vector3.up * .1f);
+            goldPopup.SetActive(true);
+
+            levelManager.Gold += goldValue;
+        }
+    }
+}
