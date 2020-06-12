4b76daac6c53202e856b37fc8f1a918ead4bc12a
Calvin C <lefrogjones@gmail.com>
2020-06-12 18:35:15 -0400

    adding more tiles, adding puffer enemy

---
+++ Assets/Scripts/Enemy/TriggerOnHit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOnHit : MonoBehaviour
+{
+    [SerializeField]
+    float growRate;
+
+    // OnDamaged event
+    public void GrowOnHit()
+    {
+        // Increase size by grow rate when hit
+        if (transform.localScale.x < 1)
+        {
+            transform.localScale = new Vector2(transform.localScale.x + growRate, transform.localScale.y + growRate);
+        }
+    }
+}
