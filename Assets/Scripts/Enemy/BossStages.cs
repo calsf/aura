7804a9d67111272596bb1c173e71b948dfee9ccd
Calvin C <lefrogjones@gmail.com>
2020-06-18 17:01:54 -0400

    adding collision and boss stage transitions

---
+++ Assets/Scripts/Enemy/BossStages.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStages : MonoBehaviour
+{
+    EnemyDefaults enemyDefaults;
+    Animator anim;
+
+    // Health to reach to activate a new stage, should be in order of highest health to lowest health
+    // Example: { 200, 100, 50} = {stage 2, stage 3, stage 4} or index + 2
+    [SerializeField]
+    int[] healthStages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyDefaults = GetComponent<EnemyDefaults>();
+        anim = GetComponent<Animator>();
+
+        anim.SetInteger("Stage", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < healthStages.Length; i++)
+        {
+            int currStage = anim.GetInteger("Stage");   // Current boss stage
+            int nextStage = (i + 2);  // Starts at stage 1, index + 2 is the stage healthStages value corresponds to
+
+            // Transition stages once lower than certain health, only move up stages, never return back to stages
+            if (enemyDefaults.HP < healthStages[i] && nextStage > currStage)
+            {
+                anim.SetInteger("Stage", nextStage);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/UpdateCollider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Updates a polygon collider to match sprite's physics shape
+
+public class UpdateCollider : MonoBehaviour
+{
+    PolygonCollider2D coll;
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<PolygonCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Reset the polygon collider
+        Vector2[] empty = { Vector2.zero };
+        for (int i = 0; i < coll.pathCount; i++)
+        {
+            coll.SetPath(i, empty);
+        }
+        coll.pathCount = sr.sprite.GetPhysicsShapeCount();
+
+        // Update the polygon collider with sprite's physics shape
+        List<Vector2> path = new List<Vector2>();
+        for (int i = 0; i < coll.pathCount; i++)
+        {
+            path.Clear();
+            sr.sprite.GetPhysicsShape(i, path);
+            coll.SetPath(i, path.ToArray());
+        }
+    }
+}
