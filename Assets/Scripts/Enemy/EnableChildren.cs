78be33fb96582e46fef7f11d6c29469c6850d9b2
Calvin C <lefrogjones@gmail.com>
2020-06-29 16:37:02 -0400

    populating l11, fixed fireball projectiles not disabling as parent

---
+++ Assets/Scripts/Enemy/EnableChildren.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Resets all children object positions and activates them on enabling this object
+
+public class EnableChildren : MonoBehaviour
+{
+    void OnEnable()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).localPosition = Vector2.zero;
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+}
